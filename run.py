e7bb5f03be320efa2b3f374ac2ef3e08807f8170
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-08 15:13:01 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
+import pickle, json, random
+import numpy as np
+from nltk.tokenize import TreebankWordTokenizer
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+
 
