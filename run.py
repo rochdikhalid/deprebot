428f5771b5020b45b17109d4d14bbf5335816ad1
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-09 12:07:18 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,21 @@ from nltk.tokenize import TreebankWordTokenizer
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
+# Instances
+tokenizer = TreebankWordTokenizer()
+lemmatizer = WordNetLemmatizer()
 
+# Setting up the data
+with open('../data/root.json') as file:
+    intents = json.load(file)
+
+# To import the preprocessed dependencies
+data = pickle.load(open( "../data/training_data", "rb" ) )
+words = data['tokenized_words']
+classes = data['categories']
+x_train = data['x_train']
+y_train = data['y_train']
+
+# To load the model
+model = load_model('../models/model.h5')
 
